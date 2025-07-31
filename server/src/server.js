f95f2d72abb238620f8db1fbb265e26a939280b3
Hendry.535230151 <hendry.535230151@stu.untar.ac.id>
2025-07-31 20:37:27 +0700

    Adding middleware and user route to server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const express = require('express');
 
 const pool = require('./configs/db');
+const errorHandler = require('./middlewares/errorHandler');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app/use('api/users', userRoute);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server run at http://localhost:${port}`);
