9e0cfe495f300331d0234fc4b88af1bba5d0a82d
Hendry.535230151 <hendry.535230151@stu.untar.ac.id>
2025-07-31 20:18:04 +0700

    Create user controller

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,101 @@
+const userService = require('../services/userServices');
+
+const getAllUsers = async (req, res, next) => {
+    try {
+        const users = userService.getAllUsers();
+        res.status(200).json({
+            message: 'Users fetch success',
+            data: users
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const getUserById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = userService.getUserById(id);
+        res.status(200).json({
+            message: `User with ID ${id} found`,
+            data: user
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const getUserByEmail = async (req, res, next) => {
+    try {
+        const { email } = req.body;
+        const user = userService.getUserByEmail(email);
+        res.status(200).json({
+            message: `User with email ${email} found`,
+            data: user
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const getUserByUsername = async (req, res, next) => {
+    try {
+        const { username } = req.body;
+        const user = userService.getUserByUsername(username);
+        res.status(200).json({
+            message: `User with username ${username} found`,
+            data: user
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const createUser = async (req, res, next) => {
+    try {
+        const { username, email, password, passwordConfirm } = req.body;
+        const user = userService.createUser(username, email, password, passwordConfirm);
+        res.status(200).json({
+            message: 'Success to create account',
+            data: user
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const updateUserPassword = async (req, res, next) => {
+    try {
+        const { email, password, passwordConfirm } = req.body;
+        const user = userService.updateUserPassword(email, password, passwordConfirm);
+        res.status(200).json({
+            message: 'Success to update password',
+            data: user
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const deleteUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = userService.deleteUser(id);
+        res.status(200).json({
+            message: 'Success to delete account',
+            data: user
+        });
+    } catch (err) {
+        next(err)
+    }
+};
+
+module.exports = {
+    getAllUsers,
+    getUserById,
+    getUserByEmail,
+    getUserByUsername,
+    createUser,
+    updateUserPassword,
+    deleteUser
+};
